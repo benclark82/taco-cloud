f962d5f5a6b773b9040aff03a67285d48a960d89
benclark82 <ben@kikilarue.com>
2020-07-28 21:11:22 -0500

    Chapter 2 changes

---
+++ src/main/java/tacos/Order.java
@@ -0,0 +1,34 @@
+package tacos;
+
+import lombok.Data;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.NotNull;
+
+@Data
+public class Order {
+    @NotNull(message="Name is required")
+    private String name;
+
+    @NotNull(message="Street is required")
+    private String street;
+
+    @NotNull(message="City is required")
+    private String city;
+
+    @NotNull(message="State is required")
+    private String state;
+
+    @NotNull(message="Zip code is required")
+    private String zip;
+
+    @Pattern(regexp="[0-9]{16}", message="Enter CC # with no spaces")
+    private String ccNumber;
+
+    @Pattern(regexp="^(0[1-9]|1[0-2])([\\/])([1-9][0-9])$",
+            message="Format must be MM/YY")
+    private String ccExpiration;
+
+    @Pattern(regexp="[0-9]{3}", message="Invalid CCV")
+    private String ccCVV;
+
+}
